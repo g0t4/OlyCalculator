36513f24c9a764b98f8587cd6843d6e28f44e7b4
Wesley McClure <wes.mcclure@gmail.com>
2012-03-23 18:38:09 -0500

    Create from initial data or defaults

--- calc.js
+++ calc.js
@@ -1,37 +1,64 @@
-function Workout() {
+function Workout(initial) {
 	var self = this;
-	self.exercises = ko.observableArray([]);
+	initial.exercises = initial.exercises || [];
+	var mapping = {
+		'exercises': {
+			create: function(options) {
+				return new Exercise(options.data);
+			}
+		}
+	}
+	ko.mapping.fromJS(initial, mapping, self);
 	self.addExercise = function() {
-		var exercise = new Exercise()
+		var exercise = new Exercise({})
 		self.exercises.push(exercise);
 	};
 	self.removeExercise = function(exercise) {
 		self.exercises.remove(exercise);
 	};
+	if (self.exercises().length < 1) {
+		Enumerable.Range(1, 3).ForEach(function(i) {
+			self.addExercise()
+		})
+	}
 }
 
-function Exercise() {
+function Exercise(initial) {
 	var self = this;
-	self.max = ko.observable(100);
-	self.barWeight = ko.observable(20.4);
-	self.sets = ko.observableArray([]);
+	initial.max = initial.max || 100;
+	initial.barWeight = initial.barWeight || 20.4;
+	initial.sets = initial.sets || []
+	var mapping = {
+		'sets': {
+			create: function(options) {
+				return new Set(self, options.data);
+			}
+		}
+	}
+	ko.mapping.fromJS(initial, mapping, self);
 	self.addSet = function() {
-		self.sets.push(new Set(self, 100));
+		self.sets.push(new Set(self, {}));
 	};
 	self.removeSet = function(set) {
 		self.sets.remove(set);
 	};
-	Enumerable.Range(1, 10).Select(function(p) {
-		return 5 * p + 50;
-	}).ForEach(function(p) {
-		self.sets.push(new Set(self, p))
-	})
+	if (self.sets().length < 1) {
+		Enumerable.Range(1, 10).Select(function(p) {
+			return 5 * p + 50;
+		}).ForEach(function(p) {
+			var initial = {
+				percent: p
+			}
+			self.sets.push(new Set(self, initial))
+		})
+	}
 }
 
-function Set(exercise, percent) {
+function Set(exercise, initial) {
 	var self = this;
-	self.percent = ko.observable(percent);
-	self.reps = ko.observable(1);
+	initial.percent = initial.percent || 100;
+	initial.reps = initial.reps || 1;
+	ko.mapping.fromJS(initial, {}, self);
 	self.weight = ko.computed(function() {
 		var weight = exercise.max() * self.percent() / 100;
 		return Math.round(weight);
@@ -45,11 +72,18 @@ function Set(exercise, percent) {
 var calc = null;
 
 $(function() {
-	calc = new Workout();
+	var init = {
+		exercises: [{
+			max: 120,
+			sets: [{
+				percent: 97
+			},{
+				percent: 98
+			}]
+		}]
+	}
+	calc = new Workout(init);
 	ko.applyBindings(calc);
-	Enumerable.Range(1, 3).ForEach(function(i) {
-		calc.addExercise()
-	})
 })
 
 // todo saving
