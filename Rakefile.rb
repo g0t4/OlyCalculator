9a0cae48b1379ea57e2a168d676b7b19a0a4db79
Wesley McClure <wes.mcclure@gmail.com>
2012-03-29 13:42:58 -0500

    Setup deploy

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,5 +2,14 @@ require 'rake'
 require 'albacore'
 
 task :build_tablet do
+	begin
+		sh 'rd android\assets /s /q'
+	rescue
+	end
 	sh 'xcopy src android\assets\www /S /I /Y'
+end
+
+task :deploy_tablet => [:build_tablet] do
+	sh'cd android & android update project -p . -t "android-14" -s'
+	sh'cd android & ant debug install -q'
 end
